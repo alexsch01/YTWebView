5f7ae24b40da1b4f3de91b951a7c1d0d45605527
Alex Schwartz <alexschwartz01@gmail.com>
2024-04-19 18:41:15 -0400

    clear data on new launch and back button works properly

--- app/src/main/java/com/alexsch01/YTWebView/MainActivity.kt
+++ app/src/main/java/com/alexsch01/YTWebView/MainActivity.kt
@@ -2,27 +2,33 @@ package com.alexsch01.YTWebView
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.KeyEvent
+import android.webkit.CookieManager
 import android.webkit.WebResourceRequest
-import android.webkit.WebSettings
+import android.webkit.WebStorage
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var myWebView: WebView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val myWebView: MyWebView = findViewById(R.id.webview)
-        val webSettings = myWebView.settings
+        myWebView = findViewById(R.id.webview)
+
+        // clear data
+        myWebView.clearFormData()
+        myWebView.clearHistory()
+        myWebView.clearSslPreferences()
+        CookieManager.getInstance().removeAllCookies(null)
+        CookieManager.getInstance().flush()
+        WebStorage.getInstance().deleteAllData()
 
         @SuppressLint("SetJavaScriptEnabled")
-        webSettings.javaScriptEnabled = true
-
-        // Enable private browsing mode
-        webSettings.cacheMode = WebSettings.LOAD_NO_CACHE
-        webSettings.domStorageEnabled = false
-        webSettings.databaseEnabled = false
+        myWebView.settings.javaScriptEnabled = true
 
         myWebView.webViewClient = object : WebViewClient() {
             override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
@@ -30,5 +36,14 @@ class MainActivity : AppCompatActivity() {
             }
         }
         myWebView.loadUrl("https://www.youtube.com")
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_BACK && myWebView.canGoBack()) {
+            myWebView.goBack()
+            return true
+        }
+
+        return super.onKeyDown(keyCode, event)
     }
 }

fd7e025085252ced612cf7877e5139390bce195e
Alex Schwartz <alexschwartz01@gmail.com>
2024-12-30 09:51:22 -0500

    use deprecated workaround for proper fullscreen video

--- app/src/main/java/com/alexsch01/YTWebView/MainActivity.kt
+++ app/src/main/java/com/alexsch01/YTWebView/MainActivity.kt
@@ -89,6 +89,16 @@ class MainActivity : AppCompatActivity() {
                 }
                 fullScreenVideoView = view
 
+                // Deprecated but this is the most stable way of making video proper fullscreen
+                fullScreenVideoView?.systemUiVisibility = (
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_FULLSCREEN
+                    or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                )
+
                 requestedOrientation = SCREEN_ORIENTATION_LANDSCAPE
                 viewGroup.addView(fullScreenVideoView, FrameLayout.LayoutParams(MATCH_PARENT, MATCH_PARENT))
             }

71a7e2753adcdd6474b3bdbf53319bc41cc9bb48
Alex Schwartz <alexschwartz01@gmail.com>
2025-01-04 19:38:47 -0500

    oop

--- app/src/main/java/com/alexsch01/youtube/CustomWebView.kt
+++ app/src/main/java/com/alexsch01/youtube/CustomWebView.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import android.webkit.WebView
 import android.util.AttributeSet
 
-class CustomWebView(Context context, AttributeSet attrs) : WebView(context, attrs) {
+class CustomWebView(context: Context, attrs: AttributeSet) : WebView(context, attrs) {
     override fun onWindowVisibilityChanged(visibility: Int) {
         super.onWindowVisibilityChanged(VISIBLE)
     }

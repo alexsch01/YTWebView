00b8cb22aed5b4b4dd6540f5515f3984a3f152a1
Alex Schwartz <alexschwartz01@gmail.com>
2025-01-05 11:43:13 -0500

    fix app launch

--- app/src/main/java/com/alexsch01/youtube/ForegroundService.kt
+++ app/src/main/java/com/alexsch01/youtube/ForegroundService.kt
@@ -1,13 +1,26 @@
 package com.alexsch01.youtube
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.Service
+import android.content.Context
 import android.content.Intent
 import android.os.IBinder
 import androidx.core.app.NotificationCompat
 
 class ForegroundService : Service() {
+    private val channelID = "foreground_service_channel"
+
+    override fun onCreate() {
+        super.onCreate()
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val channel = NotificationChannel(channelID, "Foreground Service Channel", NotificationManager.IMPORTANCE_DEFAULT)
+        notificationManager.createNotificationChannel(channel)
+    }
+
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        startForeground(1, NotificationCompat.Builder(this, "foreground_service_channel").build())
+        startForeground(1, NotificationCompat.Builder(this, channelID).build())
         return START_STICKY
     }
 

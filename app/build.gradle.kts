22415f2299d5fe1c65c4aea3f332fe164d75d7b6
Alex Schwartz <alexschwartz01@gmail.com>
2025-01-17 20:41:06 -0500

    Cleanup of unused tools (#8)
    * Update build.gradle.kts
    
    * Update libs.versions.toml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,6 @@ android {
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
@@ -35,8 +33,4 @@ android {
 dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
 }

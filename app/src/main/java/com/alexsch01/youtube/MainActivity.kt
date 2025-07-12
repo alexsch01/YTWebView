6077d2776fbbb0ed9170bc1413301a6230038591
Alex Schwartz <alexschwartz01@gmail.com>
2025-07-12 16:49:33 -0400

    remove unused import

--- app/src/main/java/com/alexsch01/youtube/MainActivity.kt
+++ app/src/main/java/com/alexsch01/youtube/MainActivity.kt
@@ -5,7 +5,6 @@ import android.content.Intent
 import android.content.pm.ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED
 import android.content.pm.ActivityInfo.SCREEN_ORIENTATION_USER_LANDSCAPE
 import android.os.Bundle
-import android.view.KeyEvent
 import android.view.View
 import android.webkit.WebChromeClient
 import android.webkit.WebResourceRequest

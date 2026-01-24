ece8a0e98ca2d868bdba0ff621d8ea9acf79eaa0
Alex Schwartz <alexschwartz01@gmail.com>
2026-01-24 13:26:24 -0500

    revert 'myWebView.setOnLongClickListener { v -> true }'

--- app/src/main/java/com/alexsch01/youtube/MainActivity.kt
+++ app/src/main/java/com/alexsch01/youtube/MainActivity.kt
@@ -31,7 +31,6 @@ class MainActivity : AppCompatActivity() {
         startService(intentForegroundService)
 
         myWebView = findViewById(R.id.webView)
-        myWebView.setOnLongClickListener { v -> true }
         myWebView.overScrollMode = WebView.OVER_SCROLL_NEVER
         myWebView.isVerticalScrollBarEnabled = false
         myWebView.settings.javaScriptEnabled = true

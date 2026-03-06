bf7331701851bcc13a3536726cfc28e5c0da8836
Alex Schwartz <alexschwartz01@gmail.com>
2026-03-06 15:10:36 -0500

    Small bug fix if activity restarts

--- app/src/main/java/com/alexsch01/youtube/MainActivity.kt
+++ app/src/main/java/com/alexsch01/youtube/MainActivity.kt
@@ -26,6 +26,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED
 
         intentForegroundService = Intent(this, ForegroundService::class.java)
         startService(intentForegroundService)

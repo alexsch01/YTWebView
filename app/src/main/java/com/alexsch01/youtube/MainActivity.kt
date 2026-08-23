73947faf0dd34cec5ea74747e042bad363053915
Alex Schwartz <alexschwartz01@gmail.com>
2026-08-22 21:13:09 -0400

    remove keepAliveInterval, too much battery

--- app/src/main/java/com/alexsch01/youtube/MainActivity.kt
+++ app/src/main/java/com/alexsch01/youtube/MainActivity.kt
@@ -129,12 +129,6 @@ class MainActivity : AppCompatActivity() {
                         )`).forEach(elem => {
                             elem.hidden = true;
                         });
-
-                        if (!window._keepAliveInterval) {
-                            window._keepAliveInterval = setInterval(() => {
-                                window.dispatchEvent(new Event('touchstart'));
-                            }, 60000);
-                        }
                     })()
                     """.trimIndent(), null)
                 }
